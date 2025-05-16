e28f6d02eb352f97da47732b5e7b3d3146f593a0
Rabina Kumari <rabina24@navgurukul.org>
2025-05-16 15:45:54 +0530

    Added simple project files

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello")
+const button = document.getElementById('colorBtn');
+
+button.addEventListener('click', () => {
+  // Random color generator
+  const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  document.body.style.backgroundColor = randomColor;
+});
